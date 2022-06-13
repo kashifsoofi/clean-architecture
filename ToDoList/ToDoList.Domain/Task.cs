0eb1fb86c2b1768c1a33c847d95d78568fe9deea
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-06-13 23:38:56 +0100

    Domain Model

---
+++ ToDoList/ToDoList.Domain/Task.cs
@@ -0,0 +1,27 @@
+﻿namespace ToDoList.Domain;
+
+public class Task
+{
+    public Guid Id { get; }
+    public string Description { get; }
+    public DateTime CreatedAt { get; }
+    public DateTime UpdatedAt { get; }
+    public DateTime DueAt { get; }
+    public bool Completed { get; }
+
+    public Task(
+        Guid id,
+        string description,
+        DateTime createdAt,
+        DateTime updatedAt,
+        DateTime dueAt,
+        bool completed)
+    {
+        Id = id;
+        Description = description;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+        DueAt = dueAt;
+        Completed = completed;
+    }
+}
---
+++ ToDoList/ToDoList.Domain/ToDoList.cs
@@ -0,0 +1,70 @@
+﻿namespace ToDoList.Domain;
+
+public class ToDoList
+{
+    public Guid Id { get; set; }
+    private List<Task> tasks;
+
+    public IEnumerable<Task> Tasks => tasks.AsReadOnly();
+
+    public ToDoList()
+    {
+        Id = Guid.NewGuid();
+        tasks = new List<Task>();
+    }
+
+    public ToDoList(Guid id, IEnumerable<Task> tasks)
+    {
+        Id = id;
+        this.tasks = tasks.ToList();
+    }
+
+    public Guid AddTask(string description, DateTime dueAt)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+        {
+            throw new ArgumentException(nameof(description), nameof(description));
+        }
+
+        if (dueAt < DateTime.UtcNow)
+        {
+            throw new ArgumentException(nameof(dueAt), nameof(dueAt));
+        }
+
+        var task = new Task(
+            Guid.NewGuid(),
+            description,
+            DateTime.UtcNow,
+            DateTime.UtcNow,
+            dueAt,
+            false);
+        tasks.Add(task);
+        return task.Id;
+    }
+
+    public void DeleteTask(Guid taskId)
+    {
+        var foundTask = tasks.FirstOrDefault(x => x.Id == taskId);
+        if (foundTask != null)
+        {
+            tasks.Remove(foundTask);
+        }
+    }
+
+    public void CompleteTask(Guid taskId)
+    {
+        var foundTask = tasks.FirstOrDefault(x => x.Id == taskId);
+        if (foundTask != null)
+        {
+            var updatedTask = new Task(
+                foundTask.Id,
+                foundTask.Description,
+                foundTask.CreatedAt,
+                DateTime.UtcNow,
+                foundTask.DueAt,
+                true);
+            tasks.Remove(foundTask);
+            tasks.Add(updatedTask);
+        }
+    }
+}
